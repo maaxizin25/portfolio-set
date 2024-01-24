c0b0b0b78f3bd48754c0d9aed411ce5b525a59cb
maaxizin25 <maaxipb25@gmail.com>
2024-01-23 23:00:23 -0300

    feat: swiper

---
+++ src/mock/portfolio-itens.ts
@@ -0,0 +1,82 @@
+export const portfolioItens = [
+  {
+    portuguese: [
+      {
+        name: "Hamburgueria 2.0",
+        description: "Lorem Ipsum Bla bla",
+        image:
+          "https://cloudfront-us-east-1.images.arcpublishing.com/estadao/77XTHHCCLBEXLC2Y5RK4PN37CE.jpg",
+        buttonView: "Visualizar",
+        buttonGit: "Código",
+        buttonVideo: "Vídeo",
+      },
+      {
+        name: "Hamburgueria 2.0",
+        description: "Lorem Ipsum Bla bla",
+        image:
+          "https://cloudfront-us-east-1.images.arcpublishing.com/estadao/77XTHHCCLBEXLC2Y5RK4PN37CE.jpg",
+        buttonView: "Visualizar",
+        buttonGit: "Código",
+        buttonVideo: "Vídeo",
+      },
+      {
+        name: "Hamburgueria 2.0",
+        description: "Lorem Ipsum Bla bla",
+        image:
+          "https://cloudfront-us-east-1.images.arcpublishing.com/estadao/77XTHHCCLBEXLC2Y5RK4PN37CE.jpg",
+        buttonView: "Visualizar",
+        buttonGit: "Código",
+        buttonVideo: "Vídeo",
+      },
+      {
+        name: "Hamburgueria 2.0",
+        description: "Lorem Ipsum Bla bla",
+        image:
+          "https://cloudfront-us-east-1.images.arcpublishing.com/estadao/77XTHHCCLBEXLC2Y5RK4PN37CE.jpg",
+        buttonView: "Visualizar",
+        buttonGit: "Código",
+        buttonVideo: "Vídeo",
+      },
+    ],
+  },
+  {
+    english: [
+      {
+        name: "Burger 2.0",
+        description: "Lorem Ipsum Bla bla",
+        image:
+          "https://cloudfront-us-east-1.images.arcpublishing.com/estadao/77XTHHCCLBEXLC2Y5RK4PN37CE.jpg",
+        buttonView: "View",
+        buttonGit: "Code",
+        buttonVideo: "Video",
+      },
+      {
+        name: "Burger 2.0",
+        description: "Lorem Ipsum Bla bla",
+        image:
+          "https://cloudfront-us-east-1.images.arcpublishing.com/estadao/77XTHHCCLBEXLC2Y5RK4PN37CE.jpg",
+        buttonView: "View",
+        buttonGit: "Code",
+        buttonVideo: "Video",
+      },
+      {
+        name: "Burger 2.0",
+        description: "Lorem Ipsum Bla bla",
+        image:
+          "https://cloudfront-us-east-1.images.arcpublishing.com/estadao/77XTHHCCLBEXLC2Y5RK4PN37CE.jpg",
+        buttonView: "View",
+        buttonGit: "Code",
+        buttonVideo: "Video",
+      },
+      {
+        name: "Burger 2.0",
+        description: "Lorem Ipsum Bla bla",
+        image:
+          "https://cloudfront-us-east-1.images.arcpublishing.com/estadao/77XTHHCCLBEXLC2Y5RK4PN37CE.jpg",
+        buttonView: "View",
+        buttonGit: "Code",
+        buttonVideo: "Video",
+      },
+    ],
+  },
+];
